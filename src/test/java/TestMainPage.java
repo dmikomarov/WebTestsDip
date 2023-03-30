671dbca7e1c968d1f3904e02620936a897a4531f
dmikomarov <77617042+dmikomarov@users.noreply.github.com>
2023-03-30 11:12:17 +0300

    добавил открытие / закрытие браузера

--- src/test/java/TestMainPage.java
+++ src/test/java/TestMainPage.java
@@ -1,18 +1,14 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import static com.codeborne.selenide.Selenide.open;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class TestMainPage extends TestData {
-
 
 
   @BeforeAll
@@ -20,26 +16,23 @@ public class TestMainPage extends TestData {
     Configuration.browserSize = System.getProperty("windowSize", "1920x1080");
     Configuration.browser = System.getProperty("browser", "chrome");
     Configuration.browserVersion = System.getProperty("version", "100.0");
-    Configuration.remote = System.getProperty("remoteUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+    Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("enableVNC", true);
     capabilities.setCapability("enableVideo", true);
     Configuration.browserCapabilities = capabilities;
     Configuration.baseUrl = "https://alfabank.ru/";
     Configuration.browserSize = "1920x1080";
-    open("https://alfabank.ru/");
+
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     //  Configuration.holdBrowserOpen = true;
   }
+@BeforeEach
+void beforeEach(){
+  open("https://alfabank.ru/");
+}
 
-  @Tag("Регресс")
-  @DisplayName("Переход на страницу 'Стать клиентом' и проверка наличия всех плиток на странице предложений")
-  @Test
-  void becomeANewCustomerOne() {
-    buttonANewCustomer();
-  }
-
-  @Tag("Регресс")
+  @Tag("Regress")
   @DisplayName("Проверка наличия популярных продуктов на главной странице")
   @Test
   void popularProducts() {
@@ -68,11 +61,20 @@ public class TestMainPage extends TestData {
     buttons();
   }
 
+  @Tag("Regress")
+  @DisplayName("Переход на страницу 'Стать клиентом' и проверка наличия всех плиток на странице предложений")
+  @Test
+  void becomeANewCustomerOne() {
+    buttonANewCustomer();
+  }
+
+
   @AfterEach
   void addAttachments() {
     Attach.screenshotAs("Last Screenshot");
     Attach.pageSource();
     Attach.browserConsoleLogs();
     Attach.addVideo();
+    Selenide.closeWebDriver();
   }
 }
